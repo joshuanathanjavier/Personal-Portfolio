429be48f18403f5d72356bbdb2edf07c5b614d86
Joshua Nathan Javier <joshuanathanjavier@gmail.com>
2026-04-22 04:53:41 +0800

    fix(projects): update Sanctuary link to correct URL

--- components/projects.tsx
+++ components/projects.tsx
@@ -11,7 +11,7 @@ function projects() {
         <h2 className="text-4xl">Projects</h2>
         <div className='flex flex-col md:flex-row items-center'>
             <div className="flex flex-col items-center m-4 p-4"> 
-            <Link className="hover:text-[#6dbfb8] hover:scale-110 transition-transform duration-300 ease-out" href="https://sanctuary-pi.vercel.app/" target="_blank" rel="noopener noreferrer"><h3 className="text-2xl flex flex-row items-center gap-1">Sanctuary<ExternalLink size="20"/></h3></Link>
+            <Link className="hover:text-[#6dbfb8] hover:scale-110 transition-transform duration-300 ease-out" href="https://sanctuarysounds.vercel.app/" target="_blank" rel="noopener noreferrer"><h3 className="text-2xl flex flex-row items-center gap-1">Sanctuary<ExternalLink size="20"/></h3></Link>
             <div className="mt-4 flex flex-col items-center md:flex-row md:space-x-4">
             <Image className="w-150 h-auto mb-4 md:mb-0" src={Sanctuary.src} alt="Sanctuary Website" width={500} height={500}/>
             </div>
